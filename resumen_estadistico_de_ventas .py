968b5a5bd07dd90f571d6303bdcd95ae27e28a3f
mparisi <mparisi7@gmail.com>
2026-09-08 21:27:41 -0300

    actualizo funcion de estadistica

--- resumen_estadistico_de_ventas .py
+++ resumen_estadistico_de_ventas .py
@@ -1,34 +1,42 @@
+# [ID_VENTA, ID_PRODUCTO, CATEGORIA, CANTIDAD, IMPORTE]
+
+# CATEGORIA:( PARA ACORDARSE)
+# 1 = Alimentos
+# 2 = Limpieza
+# 3 = Bebidas
+# 4 = Otros
+
+
+
 def resumen_estadistico(VENTAS):
+
     total_ventas = contar_ventas(VENTAS)
     promedio = promedio_ventas(VENTAS)
     mayor, menor = mayor_menor_venta(VENTAS)
 
-    print("========== ESTADISTICAS ==========")
-    print("Cantidad total de ventas:", total_ventas)
-    print("Promedio general de ventas:", promedio)
-    print("Mayor venta:", mayor)
-    print("Menor venta:", menor)
+    cant_alimentos = contar_ventas_categoria(VENTAS, 1)
+    cant_limpieza = contar_ventas_categoria(VENTAS, 2)
+    cant_bebidas = contar_ventas_categoria(VENTAS, 3)
+    cant_otros = contar_ventas_categoria(VENTAS, 4)
 
-    print("Cantidad de ventas de ALIMENTOS:",
-          contar_ventas_categoria(VENTAS, "ALIMENTOS"))
+    pct_alimentos = porcentaje_ventas_categoria(VENTAS, 1)
+    pct_limpieza = porcentaje_ventas_categoria(VENTAS, 2)
+    pct_bebidas = porcentaje_ventas_categoria(VENTAS, 3)
+    pct_otros = porcentaje_ventas_categoria(VENTAS, 4)
 
-    print("Cantidad de ventas de LIMPIEZA:",
-          contar_ventas_categoria(VENTAS, "LIMPIEZA"))
+    print(f"""========== ESTADISTICAS ==========
+Cantidad total de ventas: {total_ventas}
+Promedio general de ventas: {promedio}
+Mayor venta: {mayor}
+Menor venta: {menor}
 
-    print("Cantidad de ventas de BEBIDAS:",
-          contar_ventas_categoria(VENTAS, "BEBIDAS"))
+Cantidad de ventas de ALIMENTOS: {cant_alimentos}
+Cantidad de ventas de LIMPIEZA: {cant_limpieza}
+Cantidad de ventas de BEBIDAS: {cant_bebidas}
+Cantidad de ventas de OTROS: {cant_otros}
 
-    print("Cantidad de ventas de OTROS:",
-          contar_ventas_categoria(VENTAS, "OTROS"))
-
-    print("Porcentaje de ventas de ALIMENTOS:",
-          porcentaje_ventas_categoria(VENTAS, "ALIMENTOS"))
-
-    print("Porcentaje de ventas de LIMPIEZA:",
-          porcentaje_ventas_categoria(VENTAS, "LIMPIEZA"))
-
-    print("Porcentaje de ventas de BEBIDAS:",
-          porcentaje_ventas_categoria(VENTAS, "BEBIDAS"))
-
-    print("Porcentaje de ventas de OTROS:",
-          porcentaje_ventas_categoria(VENTAS, "OTROS"))
+Porcentaje de ventas de ALIMENTOS: {pct_alimentos}%
+Porcentaje de ventas de LIMPIEZA: {pct_limpieza}%
+Porcentaje de ventas de BEBIDAS: {pct_bebidas}%
+Porcentaje de ventas de OTROS: {pct_otros}%
+==================================""")
